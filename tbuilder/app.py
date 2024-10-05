51f6a5e7821df03a5420ff53154a307b74e951e6
Rinigus <rinigus.git@gmail.com>
2024-10-05 09:20:25 +0300

    formatting

--- tbuilder/app.py
+++ tbuilder/app.py
@@ -106,7 +106,7 @@ def main():
 
                 # if parallelized, this section has to be revised to ensure
                 # that dependencies are handled correctly
-                s.set_rpms(rpms)                
+                s.set_rpms(rpms)
                 project.update_dependencies(s, rpms_in_system)
                 print(f"{s} build successful")
 
